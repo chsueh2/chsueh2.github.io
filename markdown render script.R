84f5edf7a48b2a99beda0d6889db62862542a5f3
chsueh2 <chsueh2@ncsu.edu>
2022-07-23 19:56:06 -0400

    updated

--- markdown render script.R
+++ markdown render script.R
@@ -21,7 +21,7 @@ library(knitr)
 
 # input rmarkdown file
 # ----------------------------------------------- 
-input <- here("_Rmd", "2022-07-22-next-step.Rmd")
+input <- here("_Rmd", "2022-07-23-next-step.Rmd")
 
 
 # run the following codes to render the file
